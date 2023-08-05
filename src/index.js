094f88fc9b231f0757d7d503cc25553f96ed4890
Vincent <vincent854321@gmail.com>
2023-08-05 10:29:43 -0500

    changed to async functions

--- src/index.js
+++ src/index.js
@@ -3,20 +3,37 @@ import { key } from "./key.js";
 console.log("hi");
 
 // URL (required), options (optional)
-fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=london`)
-    .then(function (response) {
-        // Check if the response was successful (status code 200)
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+// fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=london`)
+//     .then(function (response) {
+//         // Check if the response was successful (status code 200)
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         // Parse the JSON response
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         // Successful response, data contains the weather information
+//         console.log(data);
+//     })
+//     .catch(function (err) {
+//         // Error handling
+//         console.log('Error:', err.message);
+//     });
+
+
+async function weatherData() {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=london`);
+        if (!data.ok) {
+            throw new Error("Error code 200")
         }
-        // Parse the JSON response
-        return response.json();
-    })
-    .then(function (data) {
-        // Successful response, data contains the weather information
+        const data = await response.json();
         console.log(data);
-    })
-    .catch(function (err) {
-        // Error handling
-        console.log('Error:', err.message);
-    });
+        return data;
+
+    } catch {
+        console.log("Data not retrieved");
+    }
+}
+weatherData();
--- src/loadUI.js
+++
@@ -1,3 +0,0 @@
-import { Element } from "./classes";
-
-const 
