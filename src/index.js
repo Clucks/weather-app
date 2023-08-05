43673ffcfa68f2f800b7dfa399f54d9d6c5d3de7
Vincent <vincent854321@gmail.com>
2023-08-05 14:02:47 -0500

    added location and degree

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { key } from "./key.js";
-
-console.log("hi");
+const condition = document.querySelector("#condition")
+const location = document.querySelector("#location")
+const degree = document.querySelector("#degree");
+const feels = document.querySelector("#feels");
+const wind = document.querySelector("#wind");
+const humidity = document.querySelector("#humidity");
+console.log("Weather Application loaded");
 
 // URL (required), options (optional)
 // fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=london`)
@@ -20,20 +25,43 @@ console.log("hi");
 //         // Error handling
 //         console.log('Error:', err.message);
 //     });
-
-
-async function weatherData() {
+async function getWeatherData() {
+    console.log("Fetching Weather Data");
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=london`
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=london`);
-        if (!data.ok) {
+        const response = await fetch(url);
+        if (!response.ok) {
             throw new Error("Error code 200")
         }
         const data = await response.json();
-        console.log(data);
         return data;
-
-    } catch {
-        console.log("Data not retrieved");
+    } catch (err) {
+        console.log(`Error:`, err.message);
+        throw err;
     }
 }
+
+async function weatherData() {
+    try {
+        const data = await getWeatherData();
+        loadDataToDom(data)
+        console.log(data);
+        console.log(data.current);
+        return data;
+    } catch(err) {
+        console.log("Data not retrieved");
+        console.log("Error: ", err);
+    }
+}
+
+function loadDataToDom(data) {
+    condition.textContent = data.current.condition.text; // Update the condition text
+    location.textContent = data.location.name; // Update the location name
+    degree.textContent += data.current.temp_f;
+    feels.textContent += data.current.feelslike_f;
+    wind.textContent += data.current.gust_mph;
+    humidity.textContent += data.current.humidity;
+}
+
 weatherData();
+// ... and so on
